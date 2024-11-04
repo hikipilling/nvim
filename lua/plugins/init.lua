28588081f8abda0a2cfb40e40f12c72e808c44b8
hiki <hikipilled@proton.me>
2024-11-04 12:44:49 -0500

    added nvim-ts-autotag

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,4 +2,5 @@ return {
   'tpope/vim-sleuth',
   { 'Bilal2453/luvit-meta', lazy = true },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'windwp/nvim-ts-autotag', opts = {} },
 }
