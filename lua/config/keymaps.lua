7ecc304c46d36104cb92f875323b0c69dd59e18b
hiki <hikipilled@proton.me>
2024-11-06 10:18:17 -0500

    new tab navigation keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<Tab>', 'gt', { desc = 'Go to next tab' })
+vim.keymap.set('n', '<S-Tab>', 'gT', { desc = 'Go to previous tab' })
